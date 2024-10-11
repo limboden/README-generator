1701e84cf689a65b1e59c9939c263f038d5b79ed
limboden <imbodenlu@gmail.com>
2024-10-11 15:45:31 -0400

    created a starter code that asks me what my name is and it prints it to generatedREADME.md

--- Develop/index.js
+++ Develop/index.js
@@ -1,25 +1,29 @@
 // TODO: Include packages needed for this application
 import inq from 'inquirer';
-
+import fs from 'fs';
 // TODO: Create an array of questions for user input
 const questions = [
   {
     type: 'input',
     message: 'what is your name',
-
-  },
-  {
-
-  },
-
+    name: 'name'
+  }
 ];
 
+function handleSubmit(resp) {
+  writeToFile('./generatedREADME.md', resp.name);
+}
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    err ? console.error(err) : console.log('generated!');
+  })
+}
 
 // TODO: Create a function to initialize app
 function init() {
-  inq.prompt(questions).then();
+  inq.prompt(questions).then(handleSubmit);
 }
 
 // Function call to initialize app
